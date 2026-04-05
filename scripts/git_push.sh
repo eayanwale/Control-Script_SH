ba99e44fd11a5a2af0a379961de4a938d17f1abb
Enoch Ayanwale <enochayanwale@outlook.com>
2026-04-04 21:04:48 -0700

    Update git_push.sh - add skip pipeline option and refactor into functions

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+COMMIT()
+{
+        echo "--- Pushing to origin ${branch} ---"
+        git add .
+        git commit -m "${MSG}"
+        git push -u origin ${branch}
+}
+
+MERGE()
+{
+        echo "--- Merging ${branch} into ${target} ---"
+        git checkout ${target}
+        git merge ${branch}
+        git push
+}
+
 
 branch=$(git branch --show-current)
 target=$1
@@ -9,6 +25,35 @@ if [ -z ${target} ]; then
         echo "Environments: dev, qa, uat, main"
         exit 1
 fi
+echo
+
+read -p "Skip pipeline/workflow? (y/n) " SKIP
+if [[ ${SKIP^^} == 'Y' ]]; then
+	echo "You have opted to skip and go straight to prod."
+	read -sp "Please enter G/admin password: " PASS
+	if [[ ${PASS} != "imaG" && ${PASS} != "admin" ]]; then
+        	echo "Wrong password. You are not a G or admin."
+                echo "You cannot push to prod/main. Exiting..."
+                exit 1
+        fi
+	echo
+
+	read -p "Commit message: " MSG
+	COMMIT
+	
+	echo "--- Merging ${branch} and skipping through environments ---"
+	for env in dev qa uat main
+	do
+		echo "--- Merging ${branch} into ${env} ---"
+		git checkout ${env}
+		git merge ${branch}
+		git push
+	done
+	git checkout ${branch}
+	echo "${branch} pushed and merged into ${target}."
+	exit 0
+fi
+echo
 
 if [[ ${target} == "main" ]]; then
         echo "You are about to push to prod/main..."
@@ -19,18 +64,12 @@ if [[ ${target} == "main" ]]; then
                 exit 1
         fi
 fi
+echo
 
 read -p "Commit message: " MSG
 
-echo "--- Pushing to origin ${branch} ---"
-git add .
-git commit -m "${MSG}"
-git push -u origin ${branch}
-
-echo "--- Merging ${branch} into ${target} ---"
-git checkout ${target}
-git merge ${branch}
-git push
+COMMIT
+MERGE
 
 git checkout ${branch}
 echo "${branch} pushed and merged into ${target}."
